dc6ed3d8d19775ed5046952f62ef770690f15d0c
zeusgd19 <dariusgd19@gmail.com>
2023-12-12 19:33:30 +0100

    Anagrama y correcciones

---
+++ src/Mapas/Anagrama.java
@@ -0,0 +1,37 @@
+package Mapas;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Anagrama {
+    public static void main(String[] args) throws IOException {
+        int minGroupSize = 5;
+        Map<String, ArrayList<String>> mapa = new HashMap<>();
+        BufferedReader reader = new BufferedReader(new FileReader("spanish-dict.txt"));
+        String line;
+        char[] letras;
+        ArrayList<String> lista;
+        while ((line = reader.readLine())!=null) {
+            letras = line.toCharArray();
+            Arrays.sort(letras);
+            String palabraOrdenada = new String(letras);
+            if (mapa.containsKey(palabraOrdenada)) {
+                lista = mapa.get(palabraOrdenada);
+                lista.add(line);
+                mapa.replace(palabraOrdenada,lista);
+            } else {
+                lista = new ArrayList<>();
+                mapa.put(palabraOrdenada, lista);
+            }
+        }
+        for (String clave:mapa.keySet()
+             ) {
+            if(mapa.get(clave).size() >= minGroupSize) {
+                System.out.println(clave + mapa.get(clave));
+            }
+        }
+        reader.close();
+    }
+}
