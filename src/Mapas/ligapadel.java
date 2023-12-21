5e68604a0e3a5d2d107687cad801bfced87d3cec
zeusgd19 <dariusgd19@gmail.com>
2023-12-21 13:17:04 +0100

    Modificacion LigaPadel

--- src/Mapas/ligapadel.java
+++ src/Mapas/ligapadel.java
@@ -1,6 +1,5 @@
 package Mapas;
 
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.LinkedHashMap;
 import java.util.Scanner;
@@ -9,7 +8,6 @@ public class ligapadel {
     public static void main(String[] args) {
         Map<String, Integer> mapa = new LinkedHashMap<>();
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> partidos = new ArrayList<>();
 
         String categoria;
         String equipoLocal;
@@ -19,89 +17,74 @@ public class ligapadel {
         categoria = sc.nextLine();
         int inicioLocal;
         int inicioVisitante;
-        int categorias = 1;
-        int partido = 0;
         int contarPartidos = 0;
-        int defecto = 0;
-        partidos.add(partido);
-        while(!categoria.equals("FIN")){
+
+        while (!categoria.equals("FIN")) {
             equipoLocal = sc.next();
-            if(equipoLocal.equals("FIN")){
+            if (equipoLocal.equals("FIN")) {
+                System.out.println(ganadores(mapa, contarPartidos));
                 categoria = sc.next();
-                if(categoria.equals("FIN")){
+                if (categoria.equals("FIN")) {
                     break;
                 } else {
-                    categorias++;
                     contarPartidos = 0;
-                    partido = 0;
-                    partidos.add(partido);
-                    mapa.put(categoria,-1);
+                    mapa = new LinkedHashMap<>();
                 }
                 equipoLocal = sc.next();
             }
             puntos = sc.next();
-            mapa.put(equipoLocal, mapa.getOrDefault(equipoLocal, defecto));
             equipoVisitante = sc.next();
             puntoVisitante = sc.nextLine();
-            mapa.put(equipoVisitante, mapa.getOrDefault(equipoVisitante, defecto));
-            if(Integer.parseInt(puntos) > Integer.parseInt(puntoVisitante.trim())){
+            if (Integer.parseInt(puntos) > Integer.parseInt(puntoVisitante.trim())) {
+                if (mapa.get(equipoLocal) == null) {
+                    mapa.put(equipoLocal, 0);
+                }
+                if (mapa.get(equipoVisitante) == null) {
+                    mapa.put(equipoVisitante, 0);
+                }
                 inicioLocal = mapa.get(equipoLocal) + 2;
                 inicioVisitante = mapa.get(equipoVisitante) + 1;
+                mapa.put(equipoLocal, inicioLocal);
+                mapa.put(equipoVisitante, inicioVisitante);
 
-            } else {
+            } else if (Integer.parseInt(puntos) < Integer.parseInt(puntoVisitante.trim())) {
+                if (mapa.get(equipoLocal) == null) {
+                    mapa.put(equipoLocal, 0);
+                }
+                if (mapa.get(equipoVisitante) == null) {
+                    mapa.put(equipoVisitante, 0);
+                }
                 inicioLocal = mapa.get(equipoLocal) + 1;
                 inicioVisitante = mapa.get(equipoVisitante) + 2;
+                mapa.put(equipoLocal, inicioLocal);
+                mapa.put(equipoVisitante, inicioVisitante);
 
             }
-            mapa.replace(equipoLocal,inicioLocal);
-            mapa.replace(equipoVisitante,inicioVisitante);
             contarPartidos++;
-            partidos.set(partidos.indexOf(partido),contarPartidos);
-            partido = contarPartidos;
         }
 
-        System.out.println(ganadores(categorias,mapa,partidos));
 
     }
-    public static String ganadores(int categorias, Map<String,Integer> mapa,ArrayList<Integer> partidos){
+
+    public static String ganadores(Map<String, Integer> mapa, int partidos) {
         int mayor = -1;
         String equipoGanador = "";
-        String excluye = null;
         int contarEquipos = 0;
         int partidosTotales;
         String ganadores = "";
-        for (int j = 0; j < categorias; j++) {
-            boolean encontrado = false;
-            for (String equipo:mapa.keySet()
-            ) {
-                if(excluye !=null && !excluye.equals(equipo) && !encontrado){
-                    continue;
-                }
-
-                if(excluye !=null && excluye.equals(equipo)){
-                    encontrado = true;
-                    continue;
-                }
-
-
-                if(mapa.get(equipo) != -1) {
-                    contarEquipos++;
-                    if (mapa.get(equipo) > mayor) {
-                        mayor = mapa.get(equipo);
-                        equipoGanador = equipo;
-                    } else if(mapa.get(equipo) == mayor){
-                        equipoGanador = "EMPATE";
-                    }
-                } else {
-                    excluye = equipo;
-                    mayor = -1;
-                    break;
-                }
+        for (String equipo : mapa.keySet()
+        ) {
+            contarEquipos++;
+            if (mapa.get(equipo) > mayor) {
+                mayor = mapa.get(equipo);
+                equipoGanador = equipo;
+            } else if (mapa.get(equipo) == mayor) {
+                equipoGanador = "EMPATE";
             }
-            partidosTotales = (contarEquipos * (contarEquipos - 1));
-            ganadores += equipoGanador + " " + (partidosTotales == partidos.get(j) ? 0:(partidosTotales - partidos.get(j))) + "\n";
-            contarEquipos = 0;
         }
+        partidosTotales = (contarEquipos * (contarEquipos - 1));
+        ganadores += equipoGanador + " " + (partidosTotales == partidos ? 0 : (partidosTotales - partidos));
+
         return ganadores;
     }
 }
